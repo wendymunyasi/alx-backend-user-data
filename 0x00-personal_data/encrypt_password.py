a3dc8d411ef09db89eb7b5967b13c33b2bdacc86
wendy munyasi <wendymunyasi@gmail.com>
2023-02-02 23:51:09 +0300

    Add comments to code and main.py testing files

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,11 +3,12 @@
 """
 
 
+import logging
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Hashes a password using salt.
+    """Hashes the provided password using bcrypt.
 
     Use the bcrypt package to perform the hashing (with hashpw).
 
@@ -15,20 +16,27 @@ def hash_password(password: str) -> bytes:
         password (str): Password to be hashed.
 
     Returns:
-        bytes: byte string of the hashed password.
+        bytes: A salted, hashed password in byte string format.
     """
+    # Salt and hash the password using the bcrypt package
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Validate that the provided password matches the hashed password.
+    """Validates that the provided password matches the hashed password.
 
     Args:
         hashed_password (bytes): Hashed password.
-        password (str): Password to be hashed. LOL.
+        password (str): Password to be validated.
 
     Returns:
         bool: True if the hashed password was formed from the given password,
         otherwise False.
     """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    # Try to match the hashed password with the given password
+    try:
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    # If there is an exception in the process, log the error message
+    except Exception as e:
+        logging.error("Error in password validation: {}".format(e))
+        return False
