25bcb6002efc9afba897204a323e7ef258dd17b2
wendy munyasi <wendymunyasi@gmail.com>
2023-02-02 17:52:13 +0300

    Add task 6 - Implement an is_valid function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -18,3 +18,17 @@ def hash_password(password: str) -> bytes:
         bytes: byte string of the hashed password.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate that the provided password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): Hashed password.
+        password (str): Password to be hashed. LOL.
+
+    Returns:
+        bool: True if the hashed password was formed from the given password,
+        otherwise False.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
