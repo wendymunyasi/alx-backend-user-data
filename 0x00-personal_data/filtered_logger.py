c3b2f18c7566d55626bfd046754025f68ddb733e
wendy munyasi <wendymunyasi@gmail.com>
2023-02-02 17:29:23 +0300

    Fix pycodestyle issues

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -76,14 +76,15 @@ def filter_datum(
 def get_logger() -> logging.Logger:
     """Returns a logging.Logger object.
 
-    The logger should be named "user_data" and only log up to logging.INFO level.
-    It should not propagate messages to other loggers. It should have a StreamHandler,
-    with RedactingFormatter as formatter.
+    The logger should be named "user_data" and only log up to logging.INFO
+    level.
+    It should not propagate messages to other loggers. It should have a
+    StreamHandler with RedactingFormatter as formatter.
     Create a tuple PII_FIELDS constant at the root of the module containing
-    the fields from user_data.csv that are considered PII. PII_FIELDS can contain
-    only 5 fields - choose the right list of fields that can are considered as
-    “important” PIIs or information that you must hide in your logs. Use it to
-    parameterize the formatter.
+    the fields from user_data.csv that are considered PII. PII_FIELDS can
+    contain only 5 fields - choose the right list of fields that can are
+    considered as “important” PIIs or information that you must hide in your
+    logs. Use it to parameterize the formatter.
 
     Returns:
         logging.Logger: Object.
