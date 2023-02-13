21e96ca4d1947c31cc77eae2cba1052ac195ba26
wendy munyasi <wendymunyasi@gmail.com>
2023-02-13 16:48:04 +0300

    Add task 0 - Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
-from api.v1.views import app_views
 from flask import abort, jsonify, request
+
+from api.v1.views import app_views
 from models.user import User
 
 
@@ -27,9 +28,23 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    # If the user_id is "me" and there is no current_user, return a 404 error
+    if user_id == 'me':
+        if request.current_user is None:
+            abort(404)
+        else:
+            # If the user_id is "me" and there is a current_user, return the
+            # JSON representation of the current_user
+            return jsonify(request.current_user.to_json())
+    # If user_id is None, return a 404 error
+    if user_id is None:
+        abort(404)
+    # Retrieve the user from the database using the User.get method
     user = User.get(user_id)
+    # If the user was not found, return a 404 error
     if user is None:
         abort(404)
+    # Return the JSON representation of the user
     return jsonify(user.to_json())
 
 
