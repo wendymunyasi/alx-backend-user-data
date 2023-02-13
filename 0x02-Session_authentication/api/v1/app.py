de70dfb8632f914f0101a608d616a79152674609
wendy munyasi <wendymunyasi@gmail.com>
2023-02-14 02:05:09 +0300

    Add task 10 -  Sessions in database

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -14,6 +14,7 @@ from flask_cors import CORS, cross_origin
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from api.v1.views import app_views
 
@@ -34,6 +35,8 @@ if auth_type == "session_auth":
     auth = SessionAuth()
 elif auth_type == 'session_exp_auth':
     auth = SessionExpAuth()
+elif auth_type == 'session_db_auth':
+    auth = SessionDBAuth()
 elif auth_type == "basic_auth":
     auth = BasicAuth()
 else:
