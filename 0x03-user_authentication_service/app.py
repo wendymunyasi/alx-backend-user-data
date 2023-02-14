557b3b4a8453e4a75b761279a675c9c74684542e
wendy munyasi <wendymunyasi@gmail.com>
2023-02-14 18:03:47 +0300

    Add task 7 - implement end-point 2 register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """A simple Flask app with user authentication features.
 """
-from flask import Flask, jsonify
+import logging
 
+from flask import Flask, jsonify, request
+
+from auth import Auth
+
+logging.disable(logging.WARNING)
+
+
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -13,6 +22,27 @@ def index() -> str:
         - The home page's payload.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """POST /users
+    Return:
+        - The account creation payload.
+    """
+    # Get the email and password from form data
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        # Register the user
+        AUTH.register_user(email, password)
+        # Respond with the following JSON payload:
+        # {"email": "<registered email>", "message": "user created"}
+        return jsonify({"email": email, "message": "user created"})
+    # If the user is already registered, catch the exception and return a
+    # JSON payload of the form: {"message": "email already registered"}
+    # and return a 400 status code
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
