61f053a959edc6550063d3aea5def6821dbaa0e1
wendy munyasi <wendymunyasi@gmail.com>
2023-02-14 18:33:13 +0300

    Add main.py files for tasks 2,3,5,8

---
+++ 0x03-user_authentication_service/main_3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+from db import DB
+from user import User
+
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+
+my_db = DB()
+
+email = 'test@test.com'
+hashed_password = "hashedPwd"
+
+user = my_db.add_user(email, hashed_password)
+print(user.id)
+
+try:
+    my_db.update_user(user.id, hashed_password='NewPwd')
+    print("Password updated")
+except ValueError:
+    print("Error")
---
+++ 0x03-user_authentication_service/main_8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+from auth import Auth
+
+email = 'bob@bob.com'
+password = 'MyPwdOfBob'
+auth = Auth()
+
+auth.register_user(email, password)
+
+print(auth.valid_login(email, password))
+
+print(auth.valid_login(email, "WrongPwd"))
+
+print(auth.valid_login("unknown@email", password))
