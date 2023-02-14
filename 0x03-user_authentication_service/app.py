42aa1ed87556db7c84d5dd254a1436576925e86a
wendy munyasi <wendymunyasi@gmail.com>
2023-02-14 17:36:38 +0300

    Add task 6 - Basic Flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""A simple Flask app with user authentication features.
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """GET /
+    Return:
+        - The home page's payload.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
