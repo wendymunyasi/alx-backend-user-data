c329454efaa7710d3c56abe09c75de0a5d510e93
wendy munyasi <wendymunyasi@gmail.com>
2023-02-14 16:13:34 +0300

    Add documentation to class

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,6 +10,19 @@ Base = declarative_base()
 
 
 class User(Base):
+    """A class representing a user in the system.
+
+    Attributes:
+        __tablename__ (str): The name of the table in the database where
+        user records are stored.
+        id (int): The unique identifier of the user.
+        email (str): The email address of the user.
+        hashed_password (str): The hashed password of the user.
+        session_id (str): The session ID of the user, used to maintain
+        user sessions.
+        reset_token (str): The reset token of the user, used for password
+        resets.
+    """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
